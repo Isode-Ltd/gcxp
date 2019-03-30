2f359a19f4156459f2024af0371b946b80398050
Kurt Zeilenga <kurt.zeilenga@isode.com>
2019-03-30 07:06:58 -0700

    Rework no throw checking

--- unit/codec.cpp
+++ unit/codec.cpp
@@ -10,22 +10,20 @@ BOOST_AUTO_TEST_CASE(preamble) {
     BOOST_CHECK_NO_THROW(Gcxp::checkVersion(Gcxp::gcxpVersion));
 
     std::vector<char> buffer;
-    {
-        std::size_t len;
-        BOOST_REQUIRE_NO_THROW(len = Gcxp::Codec::encodeVersion(buffer, Gcxp::gcxpVersion));
+    BOOST_CHECK_NO_THROW({
+        auto len = Gcxp::Codec::encodeVersion(buffer, Gcxp::gcxpVersion);
         BOOST_CHECK_EQUAL(len, Gcxp::Codec::preambleReadAmount);
         std::string got(std::begin(buffer), std::end(buffer));
         BOOST_CHECK_EQUAL(got.length(), Gcxp::Codec::preambleReadAmount);
         BOOST_CHECK_EQUAL(got, std::string("\xd9\xd9\xf7\x00", 4));
-    }
-    {
+    });
+    BOOST_CHECK_NO_THROW({
         auto version = ~Gcxp::gcxpVersion;
         auto pos = std::begin(buffer);
-        std::size_t len;
-        BOOST_REQUIRE_NO_THROW(len = Gcxp::Codec::decodeVersion(pos, std::end(buffer), version));
+        auto len = Gcxp::Codec::decodeVersion(pos, std::end(buffer), version);
         BOOST_CHECK_EQUAL(len, Gcxp::Codec::preambleReadAmount);
         BOOST_CHECK_NO_THROW(Gcxp::checkVersion(version));
-    }
+    });
 }
 
 BOOST_AUTO_TEST_CASE(codec) {
@@ -55,59 +53,53 @@ BOOST_AUTO_TEST_CASE(codec) {
 
     for (const auto& test : cases) {
         std::string expect(std::get<1>(test), std::get<2>(test));
-        {
+        BOOST_CHECK_NO_THROW({
             std::vector<char> buffer;
-            std::size_t len;
-            BOOST_CHECK_NO_THROW(len = Gcxp::Codec::encodeMessage(buffer, std::get<0>(test)));
+            auto len = Gcxp::Codec::encodeMessage(buffer, std::get<0>(test));
             BOOST_CHECK_EQUAL(len, expect.size());
             std::string got(std::begin(buffer), std::end(buffer));
             BOOST_CHECK_EQUAL(got, expect);
-        }
-        {
+        });
+        BOOST_CHECK_NO_THROW({
             Gcxp::Message msg;
             auto pos = std::begin(expect);
-            std::size_t len;
-            BOOST_CHECK_NO_THROW(len = Gcxp::Codec::decodeMessage(pos, std::end(expect), msg));
+            auto len = Gcxp::Codec::decodeMessage(pos, std::end(expect), msg);
             BOOST_CHECK(pos == std::end(expect));
             BOOST_CHECK_EQUAL(len, expect.size());
             BOOST_CHECK_EQUAL(msg, std::get<0>(test));
-        }
-        {
+        });
+        BOOST_CHECK_NO_THROW({
             std::vector<char> buffer;
-            std::size_t len;
-            BOOST_CHECK_NO_THROW(len = CborLite::encodeEncodedBytes(buffer, expect));
+            auto len = CborLite::encodeEncodedBytes(buffer, expect);
             BOOST_CHECK_EQUAL(len, std::get<1>(test).size());
             std::string got(buffer.begin(), buffer.end());
             BOOST_CHECK_EQUAL(got, std::get<1>(test));
-        }
-        {
+        });
+        BOOST_CHECK_NO_THROW({
             std::string got;
             auto pos = std::begin(std::get<1>(test));
-            std::size_t len;
-            BOOST_CHECK_NO_THROW(len = CborLite::decodeEncodedBytes(pos, std::end(std::get<1>(test)), got));
+            auto len = CborLite::decodeEncodedBytes(pos, std::end(std::get<1>(test)), got);
             BOOST_CHECK(pos == std::end(std::get<1>(test)));
             BOOST_CHECK_EQUAL(len, std::get<1>(test).size());
             BOOST_CHECK_EQUAL(got, expect);
-        }
-        {
+        });
+        BOOST_CHECK_NO_THROW({
             std::vector<char> buffer;
-            std::size_t len;
-            BOOST_CHECK_NO_THROW(len = CborLite::encodeEncodedBytesPrefix(buffer, expect.size()));
+            auto len = CborLite::encodeEncodedBytesPrefix(buffer, expect.size());
             auto prefix = std::get<2>(test);
             BOOST_CHECK_EQUAL(len, prefix);
             std::string got(buffer.begin(), buffer.end());
             BOOST_CHECK_EQUAL(got, std::get<1>(test).substr(0, prefix));
-        }
-        {
+        });
+        BOOST_CHECK_NO_THROW({
             std::size_t got = 0;
             auto pos = std::begin(std::get<1>(test));
             auto prefix = std::get<2>(test);
-            std::size_t len;
-            BOOST_CHECK_NO_THROW(len = CborLite::decodeEncodedBytesPrefix(pos, pos + prefix, got));
+            auto len = CborLite::decodeEncodedBytesPrefix(pos, pos + prefix, got);
             BOOST_CHECK_EQUAL(std::distance(std::begin(std::get<1>(test)), pos), prefix);
             BOOST_CHECK_EQUAL(len, prefix);
             BOOST_CHECK_EQUAL(got, expect.size());
-        }
+        });
     }
 }
 
